4dae5b0358a805e69303031b79f0ebb105414d05
Luca Carrozzo <carrozzo.lc@gmail.com>
2022-02-27 11:46:23 +0100

    24 27 30 added

--- section_27/node/routes/location.js
+++ section_27/node/routes/location.js
@@ -1,73 +1,73 @@
 const express = require('express');
 //const MongoClient = require('mongo').MongoClient;
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
 
 const router = express.Router();
 
-const url = 'mongodb+srv://new_user_luca10:xXJrd2ersNFfaJnN@cluster0.nxhns.mongodb.net/location?retryWrites=true&w=majority';
+// const url = 'mongodb+srv://new_user_luca10:xXJrd2ersNFfaJnN@cluster0.nxhns.mongodb.net/location?retryWrites=true&w=majority';
 
-const client = new MongoClient(url);
+// const client = new MongoClient(url);
 
 const locationStorage  = {
   locations: []
 };
 
 router.post('/add-location', (req, res, next) => {
-  //const id = Math.random();
+  const id = Math.random();
 
-  async function run() {
-    try {
-      // Connect the client to the server
-      await client.connect();
-      // Establish and verify connection
-      await client.db("location").command({ ping: 1 });
-      console.log("Connected successfully to server");
+  // async function run() {
+  //   try {
+  //     // Connect the client to the server
+  //     await client.connect();
+  //     // Establish and verify connection
+  //     await client.db("location").command({ ping: 1 });
+  //     console.log("Connected successfully to server");
 
-      // const db = client.db('locations');
+  //     // const db = client.db('locations');
 
-      // db.collection('user-locations').insertOne(
-      //   {
-      //     address: req.body.address, 
-      //     coords: {
-      //       lat: req.body.lat,
-      //       lng: req.body.lng
-      //     }
-      //   },
-      //   function(err, r) {
-      //     console.log('r', r)
-      //     res.json({message: 'Stored location!', locId: r.insertedId });
-      //   }
-      // );
+  //     // db.collection('user-locations').insertOne(
+  //     //   {
+  //     //     address: req.body.address, 
+  //     //     coords: {
+  //     //       lat: req.body.lat,
+  //     //       lng: req.body.lng
+  //     //     }
+  //     //   },
+  //     //   function(err, r) {
+  //     //     console.log('r', r)
+  //     //     res.json({message: 'Stored location!', locId: r.insertedId });
+  //     //   }
+  //     // );
 
-      const doc = {
-        address: req.body.address, 
-        coords: {
-          lat: req.body.lat,
-          lng: req.body.lng
-        }
-      };
-      const result = await collection('user-locations').insertOne(doc);
-      console.log(
-        `A document was inserted with the _id: ${result.insertedId}`,
-      );
-      res.json({message: 'Stored location!', locId: r.insertedId });
+  //     const doc = {
+  //       address: req.body.address, 
+  //       coords: {
+  //         lat: req.body.lat,
+  //         lng: req.body.lng
+  //       }
+  //     };
+  //     const result = await collection('user-locations').insertOne(doc);
+  //     console.log(
+  //       `A document was inserted with the _id: ${result.insertedId}`,
+  //     );
+  //     res.json({message: 'Stored location!', locId: r.insertedId });
 
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
-
-  // locationStorage.locations.push({
-  //   id: id, 
-  //   address: req.body.address, 
-  //   coords: {
-  //     lat: req.body.lat,
-  //     lng: req.body.lng
+  //   } finally {
+  //     // Ensures that the client will close when you finish/error
+  //     await client.close();
   //   }
-  // });
-  //res.json({message: 'Stored location!', locId: id});
+  //}
+  //run().catch(console.dir);
+
+  locationStorage.locations.push({
+    id: id, 
+    address: req.body.address, 
+    coords: {
+      lat: req.body.lat,
+      lng: req.body.lng
+    }
+  });
+  res.json({message: 'Stored location!', locId: id});
 });
 
 router.get('/location/:lid', (req, res, next) => {
